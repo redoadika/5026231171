d81700164392c4704f69ba95fa11b5dc850d7aba
redoadika <redodharmawan@gmail.com>
2025-05-21 12:39:18 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 // import java.io :
 // System.out.println() :
@@ -55,3 +57,15 @@ Route::get('template1', function () {
 Route::get('tugaslinktree', function () {
 	return view('tugaslinktree');
 });
+
+Route::get('dosen', [Link::class,'index'] );
+
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index'] );
+
+Route::get('/formulir', [PegawaiController::class,'formulir'] );
+Route::post('/formulir/proses', [PegawaiController::class,'proses'] );
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
